5ba209aabe6044689ef3f1c01004ed73b81347a8
Tom Benbow <tom@hellomoon.net>
2022-04-19 16:22:05 -0500

    Add microgen updates with ipfs fixes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withSvgr = require("next-svgr");
-
-module.exports = withSvgr({
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-      },
-    ];
-  },
-});
+/* next.config.js  */
+module.exports = {
+};
